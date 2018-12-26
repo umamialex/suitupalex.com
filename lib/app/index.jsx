37059759a9148aaa8472c6f498e6d8d0c00bd9a2
Alexander Martin <alex@suitupalex.com>
2018-12-26 15:49:30 -0500

    app: Confetti!

--- lib/app/index.jsx
+++ lib/app/index.jsx
@@ -5,42 +5,82 @@ import {
   Container,
   Header,
 } from 'semantic-ui-react'
+import Confetti from 'react-confetti'
 import React from 'react'
 import fitty from 'fitty'
 
 class App extends React.Component {
-  componentDidMount() {
-    fitty('.fit', {
-      maxSize: 130,
+  state = {}
+
+  getWindowDimensions = () => {
+    this.setState({
+      height: window.innerHeight,
+      width: window.innerWidth,
     })
   }
 
+  componentDidMount() {
+    fitty('.fit', {
+      maxSize: 90,
+    })
+
+    this.getWindowDimensions()
+  }
+
   render() {
-    return (
-      <Container>
-        <div
-          style={{
-            alignItems: 'stretch',
-            display: 'flex',
-            flexDirection: 'column',
-            height: '100vh',
-            justifyContent: 'center',
+    const {
+      state,
+    } = this
+
+    const {
+      height,
+      width,
+    } = state
+
+    const confetti = width && height
+      ? <Confetti
+          colors={['#00f']}
+          confettiSource={{
+            h: 0,
+            w: width,
+            x: 0,
+            y: 0,
           }}
-        >
-          <div>
-            <div
-              className='fit'
-              style={{
-                color: '#00f',
-                fontFamily: 'PT Serif',
-                lineHeight: 'normal',
-                marginBottom: 0,
-              }}
-            >SuitUpAlex</div>
+          gravity={0.03}
+          height={height}
+          numberOfPieces={10}
+          width={width}
+        />
+      : null
+
+    return (
+      <div>
+        {confetti}
+        <Container>
+          <div
+            style={{
+              alignItems: 'stretch',
+              display: 'flex',
+              flexDirection: 'column',
+              height: '100vh',
+              justifyContent: 'center',
+            }}
+          >
+            <div>
+              <div
+                className='fit'
+                style={{
+                  color: '#00f',
+                  fontFamily: 'PT Serif',
+                  lineHeight: 'normal',
+                  marginBottom: 0,
+                }}
+              >SuitUpAlex</div>
+            </div>
+            <div>&nbsp;Alexander Martin</div>
           </div>
-          <div>&nbsp;Alexander Martin</div>
-        </div>
-      </Container>
+        </Container>
+      </div>
     )
   }
 }
