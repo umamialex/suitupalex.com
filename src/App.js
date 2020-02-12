630a9d91ef2d5578ff55241f612200d8fc06ceed
Alexander Martin <alex@suitupalex.com>
2020-02-11 23:22:59 -0600

    src: Initial implementation.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled from 'styled-components'
+import hawtie from './hawtie.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Hawtie
+      src={hawtie}
+    />
+  )
 }
 
-export default App;
+const Hawtie = styled.img`
+  height: 100%;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+`
+
+export default App
