e1c9f7489be59ab03a86c87edd1980367079f3af
Alexander Martin <alex@suitupalex.com>
2020-03-28 02:19:33 -0500

    src: App: Implement the polaroid.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,88 @@
 import React from 'react'
+import {
+  Paper,
+} from '@material-ui/core'
+import {
+  red,
+} from '@material-ui/core/colors'
+import {
+  ThemeProvider,
+  createMuiTheme,
+} from '@material-ui/core/styles'
 import styled from 'styled-components'
 import hawtie from './hawtie.jpg'
 
+const theme = createMuiTheme({
+  palette: {
+    background: {
+      paper: 'rgba(255, 255, 255, 0.5)',
+    },
+  },
+})
+
 function App() {
   return (
-    <Hawtie
-      src={hawtie}
-    />
+    <ThemeProvider
+      theme={theme}
+    >
+      <Page>
+        <Container>
+          <Polaroid
+            elevation={3}
+            square
+          >
+            <Hawtie
+              src={hawtie}
+            />
+            <Marker>
+              <Initials>A.M.</Initials>
+              <BirthYear>1991</BirthYear>
+            </Marker>
+          </Polaroid>
+        </Container>
+      </Page>
+    </ThemeProvider>
   )
 }
 
+const Page = styled.div`
+  align-items: center;
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+`
+
+const Container = styled.div`
+  display: inline-flex;
+  max-width: 40em;
+  padding: 3em;
+`
+
+const Polaroid = styled(Paper)`
+  background-color: rgba(255, 255, 255, 0.25);
+  padding: 2.5em 3em 1em 3em;
+`
+
 const Hawtie = styled.img`
-  height: 100%;
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  right: 0;
+  box-sizing: border-box;
+  width: 100%;
+`
+
+const Marker = styled.div`
+  color: ${red.A400};
+  display: flex;
+  flex-direction: row;
+  font-family: VT323;
+  font-size: 3em;
+  justify-contnet: space-between;
+`
+
+const Initials = styled.div`
+`
+
+const BirthYear = styled.div`
+  align-self: flex-end;
+  margin-left: auto;
 `
 
 export default App
