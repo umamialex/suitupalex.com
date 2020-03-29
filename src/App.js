24908b4c8187d512caf9531d63874f2f1fe0fdf8
Alexander Martin <alex@suitupalex.com>
2020-03-29 04:06:57 -0500

    src: App: Final redesign <3 initial implementation

--- src/App.js
+++ src/App.js
@@ -1,80 +1,203 @@
 import React from 'react'
 import {
-  Paper,
-} from '@material-ui/core'
-import {
+  amber,
+  deepPurple,
   indigo,
+  pink,
+  red,
 } from '@material-ui/core/colors'
 import {
   ThemeProvider,
-  createMuiTheme,
 } from '@material-ui/core/styles'
 import styled from 'styled-components'
-import hawtie from './hawtie.jpg'
-
-const theme = createMuiTheme({
-  palette: {
-    background: {
-      paper: 'rgba(255, 255, 255, 0.6)',
-    },
-  },
-})
+import hawtie from './hawtie.png'
 
 function App() {
   return (
-    <ThemeProvider
-      theme={theme}
-    >
+    <ThemeProvider>
       <Page>
-        <Container>
-          <Polaroid
-            elevation={3}
-            square
+        <Info>
+          <InfoContainer>
+            <Name>
+              A
+              <Thirteen>
+                13
+              </Thirteen>
+              x&nbsp;&nbsp;&nbsp;&nbsp;Martin
+            </Name>
+            <HawtieContainer>
+              <HawtieBackground />
+              <BigHeart
+                viewBox="0 0 32 29.6"
+              >
+                <path d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z"/>
+              </BigHeart>
+              <AnimatedHeart
+                viewBox="0 0 32 29.6"
+              >
+                <path d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z"/>
+              </AnimatedHeart>
+              <Heart
+                viewBox="0 0 32 29.6"
+              >
+                <path d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z"/>
+              </Heart>
+              <Hawtie
+                src={hawtie}
+              />
+            </HawtieContainer>
+          </InfoContainer>
+        </Info>
+        <Boxes>
+          <Box
+            color={indigo}
           >
-            <Hawtie
-              src={hawtie}
-            />
+            <Polaroid />
             <Marker>
               <Initials>A.M.</Initials>
               <BirthYear>1991</BirthYear>
             </Marker>
-          </Polaroid>
-        </Container>
+          </Box>
+          <Box
+            color={amber}
+          />
+          <Box
+            color={deepPurple}
+          />
+          <Box
+            color={pink}
+          />
+        </Boxes>
       </Page>
     </ThemeProvider>
   )
 }
 
 const Page = styled.div`
-  align-items: center;
   display: flex;
+  padding-right: 15vw;
+  justify-content: space-between;
+`
+
+const Info = styled.div`
+  align-items: flex-end;
+  align-self: flex-start;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  position: sticky;
+  top: 0em;
   height: 100vh;
-  justify-content: center;
 `
 
-const Container = styled.div`
-  display: inline-flex;
-  max-width: 40em;
-  padding: 3em;
+const InfoContainer = styled.div`
+  align-items: flex-end;
+  display: flex;
+  flex-direction: column;
+  margin-right: 5em;
+  margin-bottom: 0em;
 `
 
-const Polaroid = styled(Paper)`
-  background-color: rgba(255, 255, 255, 0.25);
-  padding: 2.5em 3em 1em 3em;
+const Name = styled.div`
+  font-family: VT323;
+  font-size: 4em;
+  margin-bottom: 1rem;
+  text-transform: uppercase;
+`
+
+const Thirteen = styled.div`
+  background-color: ${red.A400};
+  color: #fff;
+  display: inline-block;
+`
+
+const Heart = styled.svg`
+  fill: #fff;
+  left: calc(50% + 4em);
+  position: absolute;
+  top: 50%;
+  width: 4em;
+`
+
+const AnimatedHeart = styled(Heart)`
+  animation: Pulse 2s ease infinite;
+`
+
+const BigHeart = styled(Heart)`
+  fill: ${indigo.A400};
+  left: calc(50% - 3em);
+  top: calc(50% - 6em);
+  width: 16em;
+  transform: rotate(10deg);
+`
+
+const HawtieContainer = styled.div`
+  position: relative;
+  right: 3.5em;
+  text-align: center;
+  top: -7em;
+`
+
+const HawtieBackground = styled.div`
+  clip-path: 
+    polygon(
+      0% 10%,    /* top left */
+      15% 0%,    /* top left */
+      95% 0%,    /* top right */
+      100% 0%,   /* top right */
+      100% 90%,  /* bottom right */
+      85% 100%,  /* bottom right */
+      0% 100%,   /* bottom left */
+      0 95%      /* bottom left */
+    );
+  position: absolute;
+  height: calc(100% - 8em);
+  width: 100%;
+  box-sizing: border-box;
+  margin: 3em 0;
 `
 
 const Hawtie = styled.img`
+  margin: 0 1em;
+  position: relative;
+  width: 15em;
+`
+
+const Boxes = styled.div`
+  align-self: flex-end;
+  display: inline-flex;
+  flex-direction: column;
+  width: 60em;
+  max-width: 100%;
+`
+
+const Box = styled.div`
+  align-items: center;
+  animation: GradientBackground 20s ease infinite;
+  background: linear-gradient(270deg, ${(p) => p.color.A100}, ${(p) => p.color.A200});
+  background-size: 400% 400%;
   box-sizing: border-box;
-  width: 100%;
+  display: flex;
+  flex-direction: column;
+  height: 75vh;
+  padding: 5vh;
+`
+
+const Polaroid = styled.div`
+  flex-grow: 1;
 `
 
 const Marker = styled.div`
-  color: ${indigo.A400};
+  color: #fff;
   display: flex;
   flex-direction: row;
   font-family: VT323;
-  font-size: 3em;
-  justify-contnet: space-between;
+  font-size: 2em;
+  justify-content: space-between;
+  margin-top: 0.25em;
+  width: 100%;
+  flex-shrink: 0;
 `
 
 const Initials = styled.div`
